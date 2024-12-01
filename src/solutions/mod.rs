cb2de8fbcbcf75acad8a28ee978f5b6d01a08ed1
Imanol Gomez <imanol@steinergomez.com>
2024-12-01 17:36:21 +0100

    Initial project setup

---
+++ src/solutions/mod.rs
@@ -0,0 +1,9 @@
+// src/solutions/mod.rs
+
+// Make modules public so they can be used from main.rs
+pub mod day01;
+// pub mod day02;
+// pub mod day03;
+// etc...
+
+// You'll uncomment each day's module as you implement it
---
+++ src/utils/mod.rs
@@ -0,0 +1,4 @@
+// src/utils/mod.rs
+
+pub mod input;
+// Add other utility modules here as needed
